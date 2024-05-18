80e3cee5e0e0c86bd3a66f39b688636d1de6fa96
Aristote ENGUDI <engudiaristot@gmail.com>
2024-05-18 18:55:03 +0100

    change api directory

---
+++ src/components/api/meteo.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import CONFIG from "../../configs/config";
+
+// WeatherApi EndPoint
+const forecastEndpoint = params => `http://api.weatherapi.com/v1/forecast.json?key=${CONFIG.API_KEY}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`;
+const locationsEndpoint = params => `http://api.weatherapi.com/v1/search.json?key=${CONFIG.API_KEY}&q=${params.cityName}`;
+
+// Call to Api Endpoint and do de query
+const apiCall = async (endpoint) =>{
+    const options = {
+        method: "GET",
+        url: endpoint
+    }
+
+    try{
+        const responses = await axios.request(options);
+        return responses.data;     
+    }catch(exception){
+        console.log('Exception',exception);
+        return null;
+    }
+}
+
+export const fetchWeatherForecast = params => {
+    return apiCall(forecastEndpoint(params));
+}
+
+export const fetchWeatherLocations = params => {
+    return apiCall(locationsEndpoint(params));
+}
